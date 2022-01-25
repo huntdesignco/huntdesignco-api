c50af96af1fe46e7abcb017d3aed0994d898524e
William Hunt <will@huntdesignco.com>
2022-01-25 01:10:18 -0500

    corrected site urls

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'xnizstudio'),
-        username: env('DATABASE_USERNAME', 'xnizstudio'),
-        password: env('DATABASE_PASSWORD', 'xnizStudio123!'),
+        database: env('DATABASE_NAME', 'huntdesignco'),
+        username: env('DATABASE_USERNAME', 'huntdesignco'),
+        password: env('DATABASE_PASSWORD', 'j?3_Se5C+yM@'),
         ssl: env.bool('DATABASE_SSL', true),
       },
       options: {}
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.xnizstudio.com',
+  url: 'https://api.huntdesignco.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '3644f6535236792855b6ba9770cd5f60'),
